ddfae59e17898018109b02192ad05da36e90dc63
Hubert_wu <hubert2047@gmail.com>
2023-09-16 21:10:16 +0800

    af

--- src/App.js
+++ src/App.js
@@ -6,26 +6,22 @@ function App() {
     return (
         <div className='App'>
             <Helmet>
-                {/* <title>react-share11</title>
-                <meta http-equiv='cleartype' content='on'></meta>
-                <meta name='description' content='hello'></meta>
+                {/* <meta http-equiv='cleartype' content='on'></meta> */}
                 <meta property='og:type' content='website' />
-                <meta property='og:image:secure_url' content='https://chat.openai.com/images/chatgpt-share-og.png' />
-                <meta property='og:site_name' content='npm'></meta>
-                <meta name='msapplication-TileColor' content='#cb3837'></meta>
-                <meta
-                    name='msapplication-TileImage'
-                    content='http://static-production.npmjs.com/7a7ffabbd910fc60161bc04f2cee4160.png'></meta>
-                <meta
-                    name='msapplication-config'
-                    content='http://static-production.npmjs.com/668aac888e52ae13cac9cfd71fabd31f.xml'></meta>
                 <meta property='og:title' content='react-share11'></meta>
-                <meta property='og:site_name' content='npm'></meta>
-                <meta name='keywords' content='rsocial-media,share,button,count'></meta>
-                <meta
-                    property='og:description'
-                    content='Socialeact.. Latest version: 4.4.1, last published: a year ago. Start using react-share in your project by running `npm i react-share`. There are 357 other projects in the npm registry using react-share.'></meta>
-                <meta property='og:url' content='https://hubert2047.github.io/react-share/' /> */}
+                <meta property='og:description' content='Socialeact...'></meta>
+                <meta property='og:image' content='http://chat.openai.com/images/chatgpt-share-og.png' />
+                <meta property='og:url' content='https://hubert2047.github.io/react-share/' />
+                {/* <meta property='og:site_name' content='npm'></meta> */}
+                {/* <meta name='msapplication-TileColor' content='#cb3837'></meta> */}
+                {/* <meta
+                    name='msapplication-TileImage'
+                    content='http://static-production.npmjs.com/7a7ffabbd910fc60161bc04f2cee4160.png'></meta> */}
+                {/* <meta
+                    name='msapplication-config'
+                    content='http://static-production.npmjs.com/668aac888e52ae13cac9cfd71fabd31f.xml'></meta> */}
+                {/* <meta property='og:site_name' content='npm'></meta> */}
+                {/* <meta name='keywords' content='rsocial-media,share,button,count'></meta> */}
             </Helmet>
             <FacebookShareButton url='https://hubert2047.github.io/react-share/'>
                 <FacebookIcon></FacebookIcon>
