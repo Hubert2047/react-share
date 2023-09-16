bb15008f153615dd7dc7de1ef76680e25c049edb
Hubert_wu <hubert2047@gmail.com>
2023-09-16 16:37:03 +0800

    add /

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className='App'>
             <Helmet>
-                <title>react-share</title>
+                <title>react-share11</title>
                 <meta name='description' content='hello'></meta>
                 <meta
                     property='og:image'
@@ -21,19 +21,16 @@ function App() {
                     data-react-helmet='true'
                     name='msapplication-config'
                     content='http://static-production.npmjs.com/668aac888e52ae13cac9cfd71fabd31f.xml'></meta>
-                <meta data-react-helmet='true' property='og:title' content='react-share'></meta>
+                <meta data-react-helmet='true' property='og:title' content='react-share11'></meta>
                 <meta data-react-helmet='true' property='og:site_name' content='npm'></meta>
-                <meta
-                    data-react-helmet='true'
-                    name='keywords'
-                    content='react,component,react-component,social,media,social-media,share,button,count'></meta>
+                <meta data-react-helmet='true' name='keywords' content='rsocial-media,share,button,count'></meta>
                 <meta
                     data-react-helmet='true'
                     property='og:description'
-                    content='Social media share buttons and share counts for React.. Latest version: 4.4.1, last published: a year ago. Start using react-share in your project by running `npm i react-share`. There are 357 other projects in the npm registry using react-share.'></meta>
+                    content='Socialeact.. Latest version: 4.4.1, last published: a year ago. Start using react-share in your project by running `npm i react-share`. There are 357 other projects in the npm registry using react-share.'></meta>
                 <meta property='og:url' content={currentURL} />
             </Helmet>
-            <FacebookShareButton>
+            <FacebookShareButton url={currentURL}>
                 <FacebookIcon></FacebookIcon>
             </FacebookShareButton>
         </div>
